e879b03f229d99a2c60bcdf63cb0840188830858
Allan Renucci <allan.renucci@gmail.com>
2017-06-11 20:21:50 +0200

    Use LUB in macro definition

--- src/main/scala/scala/xml/quote/quote.scala
+++ src/main/scala/scala/xml/quote/quote.scala
@@ -6,7 +6,7 @@ import scala.xml.quote.internal.QuoteImpl
 package object quote {
   implicit class XmlQuote(ctx: StringContext) {
     object xml {
-      def apply[T](args: T*): AnyRef = macro QuoteImpl.apply[T]
+      def apply[T](args: T*): Seq[Node] = macro QuoteImpl.apply[T]
     }
   }
 }
